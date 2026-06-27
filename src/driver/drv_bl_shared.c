f98eb2f397d086d3718a53106101b678194503ef
ops-internal-lab <ops-internal@ohmicreach.com>
2026-06-27 06:49:11 +0200

    Add files via upload

--- src/driver/drv_bl_shared.c
+++ src/driver/drv_bl_shared.c
@@ -211,8 +211,9 @@ static int base64_encode(const unsigned char *in, int len, char *out) {
     return o;
 }
 
-void BL09XX_AppendInformationToHTTPIndexPage(http_request_t *request)
+void BL09XX_AppendInformationToHTTPIndexPage(http_request_t *request, int bPreState)
 {
+	(void)bPreState;
     // Dashboard migrated to standalone JSON architecture on /dash
 }
 
@@ -408,7 +409,6 @@ float BL_ChangeEnergyUnitIfNeeded(float Wh) {
 
 void BL_ProcessUpdate(float voltage, float current, float power, float frequency, float energyWh) {
     int i;
-    int xPassedTicks;
     time_t ntpTime;
     struct tm *ltm;
     char datetime[64];
@@ -966,10 +966,14 @@ int http_fn_api_dash(http_request_t *request) {
 
         if (volt_v > 0xFFFF) volt_v = 0xFFFF;
         if (curr_v > 0xFFFF) curr_v = 0xFFFF;
-        if (pwr_v  >  32767) pwr_v  =  32767; if (pwr_v  < -32768) pwr_v  = -32768;
-        if (cpwr_v >  32767) cpwr_v =  32767; if (cpwr_v < -32768) cpwr_v = -32768;
-        if (bal_v  >  32767) bal_v  =  32767; if (bal_v  < -32768) bal_v  = -32768;
-        if (est_v  >  32767) est_v  =  32767; if (est_v  < -32768) est_v  = -32768;
+        if (pwr_v  >  32767) pwr_v  =  32767;
+        if (pwr_v  < -32768) pwr_v  = -32768;
+        if (cpwr_v >  32767) cpwr_v =  32767;
+        if (cpwr_v < -32768) cpwr_v = -32768;
+        if (bal_v  >  32767) bal_v  =  32767;
+        if (bal_v  < -32768) bal_v  = -32768;
+        if (est_v  >  32767) est_v  =  32767;
+        if (est_v  < -32768) est_v  = -32768;
         if (lms_v  > 0xFFFF) lms_v  = 0xFFFF;
 
         raw[0]  = (unsigned char)(volt_v  & 0xFF);
